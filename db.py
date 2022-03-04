f5788257d6b689ed4560b0398fdd1386cfe3aa0b
Yintc <yiqazwsx123@gmail.com>
2022-03-04 11:55:33 +0800

    modify the information of the database, taipei_scene, for ubuntu

--- db.py
+++ db.py
@@ -1,10 +1,8 @@
 from mysql.connector.pooling import MySQLConnectionPool
 
 con_pool=MySQLConnectionPool(
-    host="localhost",
-    port="3400",
     user="root",
-    password="abc123456",
+    password="AbC123456",
     database="Taipei_Scene",
     pool_name="myPool",
     pool_size=20,

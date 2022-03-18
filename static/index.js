656a2634e81f8522b405eecf2d17d1e431e98e6e
Yintc <yiqazwsx123@gmail.com>
2022-03-18 10:37:13 +0800

    update index.js on pressing enter to click the button searching attractions.(debug)

--- static/index.js
+++ static/index.js
@@ -86,7 +86,8 @@ function clean_content(){
 
 //--------------------------------監聽事件-------------------------------//
 window.addEventListener("keydown", function(e){
-    if(e.code=="Enter" || e.code=="NumpadEnter"){
+    let search=document.getElementById("search");
+    if(search.hasFocuse() && (e.code=="Enter" || e.code=="NumpadEnter")){
         button.click();
     }
 });

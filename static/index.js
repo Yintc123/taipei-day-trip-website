b299700a941407c295ffd383ab94b64dd74d585b
Yintc <yiqazwsx123@gmail.com>
2022-03-18 10:16:13 +0800

    update index.js on pressing enter to click the button searching attractions.(debug)

--- static/index.js
+++ static/index.js
@@ -87,6 +87,7 @@ function clean_content(){
 //--------------------------------監聽事件-------------------------------//
 window.addEventListener("keydown", function(e){
     console.log(e.code);
+    console.log(e);
     if(e.code==13){//enter的key.code為13
         console.log("1");
         button.click();

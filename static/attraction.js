dc080c472bf30d8001fce91fc34559e730f60f41
Yintc <yiqazwsx123@gmail.com>
2022-03-31 12:27:11 +0800

    debug

--- static/attraction.js
+++ static/attraction.js
@@ -137,16 +137,16 @@ booking_button.addEventListener("click", function(){
     if(user_status==0){
         sign_in_or_up.click();
     }
-    // else{
-    //     import("./booking_module.js").then(func => {
-    //         func.booking_tour(id);
-    //         window.location=func.booking;
-    //     });
-    // }
-    import("./booking_module.js").then(func => {
-        func.booking_tour(id);
-        window.location=func.booking;
-    }); 
+    else{
+        import("./booking_module.js").then(func => {
+            func.booking_tour(id);
+            window.location=func.booking;
+        });
+    }
+    // import("./booking_module.js").then(func => {
+    //     func.booking_tour(id);
+    //     window.location=func.booking;
+    // }); 
 })
 
 last.addEventListener("click", function(){

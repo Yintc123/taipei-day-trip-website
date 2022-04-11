a0467b78f7f35bf0d218fa7d15a9970d0abfd65d
Yintc <yiqazwsx123@gmail.com>
2022-04-11 14:58:38 +0800

    debug modifying the page, thankyou, for better experiences in using phones.

--- static/thankyou.js
+++ static/thankyou.js
@@ -96,6 +96,7 @@ function create_table_comp(e, order_index){
     for(index in order_list){
         let td=document.createElement("td");
         td.textContent=order_list[index];
+        console.log(hidden.indexOf(index));
         if (hidden.indexOf(index)!=-1){
             console.log(index);
             td.className="order_hidden";

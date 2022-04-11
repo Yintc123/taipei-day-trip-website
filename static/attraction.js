6409a01d7696d308091264c0a7696a08e87634de
Yintc <yiqazwsx123@gmail.com>
2022-04-11 19:09:26 +0800

    debug modifying the page, thankyou, for better experiences in using phones.

--- static/attraction.js
+++ static/attraction.js
@@ -141,7 +141,7 @@ function set_date(data, user){
         let order_date=document.getElementById("order_date");
         order_date.textContent=data["data"]["trip"]["date"];
         calendar.style.display="none";
-        order_date.style.display="static";
+        order_date.style.display="inline-block";
     }
     // let date=new Date();
     // let day=date.getDate();

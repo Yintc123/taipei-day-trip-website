b9d14cd79fbc9d699defc4a2ba35a594fc3009c8
Yintc <yiqazwsx123@gmail.com>
2022-04-12 12:30:43 +0800

    make the web better.

--- static/attraction.js
+++ static/attraction.js
@@ -367,6 +367,7 @@ function get_order_number(){
     let order_number=window.location.href;
     if(window.location.href.indexOf("ordernumber")!=-1){
         order_number=order_number.split("=")[1];
+        order_number=order_number.split("&")[0];
         order_flag=1;
         return order_number;
     }else{

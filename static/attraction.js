1e58a31e64b75bf782cfa1e080f725de50ed9510
Yintc <yiqazwsx123@gmail.com>
2022-04-12 12:16:12 +0800

    make the web better.

--- static/attraction.js
+++ static/attraction.js
@@ -38,7 +38,7 @@ function init(){
                         import("./order_module.js").then(func=>{
                             func.get_order_info(order_number).then(result=>{
                                 if(result["data"]==null){
-                                    window.location=url_attraction+id;
+                                    window.location=url_home;
                                 }
                                 console.log(result);
                                 if(result["data"]["contact"]["email"]==user["data"]["email"]){

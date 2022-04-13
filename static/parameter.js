45882fd6c1a1b71883f6613679edd8361b128f08
Yintc <yiqazwsx123@gmail.com>
2022-04-13 19:56:04 +0800

    test env.

---
+++ static/parameter.js
@@ -0,0 +1,16 @@
+let data={
+    "develop":{
+        'url_home':'http://127.0.0.1:3000/',
+        'url_attraction':'http://127.0.0.1:3000/attraction/',
+        'url_booking':'http://127.0.0.1:3000/booking',
+        'url_api_attraction':'http://127.0.0.1:3000/api/attraction/'
+    },
+    "production":{
+        'url_home':'http://3.115.234.130:3000/',
+        'url_attraction':'http://3.115.234.130:3000/attraction/',
+        'url_booking':'http://3.115.234.130:3000/booking',
+        'url_api_attraction':'http://3.115.234.130:3000/api/attraction/'
+    }
+}
+
+module.exports={data}

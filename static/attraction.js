55dfe37afdd3b6eb1c265d53ba7514e2f3570ce8
Yintc <yiqazwsx123@gmail.com>
2022-04-13 19:59:32 +0800

    test env.

--- static/attraction.js
+++ static/attraction.js
@@ -9,9 +9,9 @@ console.log("hi");
 // let booking="http://3.115.234.130:3000/booking";//EC2
 // const env='develop';
 const env='production';
-let result=require('./parameter.js');
+let data=require('./parameter.js');
 
-let url_home='http://3.115.234.130:3000/';//EC2
+let url_home=import('./parameter.js').then();//EC2
 let url_api_attraction="http://3.115.234.130:3000/api/attraction/";//EC2
 let url_attraction="http://3.115.234.130:3000/attraction/";//EC2
 let booking="http://3.115.234.130:3000/booking";//EC2

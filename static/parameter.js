8be60c9bf07e6333607360d96e457054cce51f6f
Yintc <yiqazwsx123@gmail.com>
2022-04-13 20:15:54 +0800

    test env.

--- static/parameter.js
+++ static/parameter.js
@@ -1,4 +1,4 @@
-let data2={
+export default data2={
     "develop":{
         'url_home':'http://127.0.0.1:3000/',
         'url_attraction':'http://127.0.0.1:3000/attraction/',
@@ -13,4 +13,4 @@ let data2={
     }
 }
 
-module.exports={data}
+// export default{data2}

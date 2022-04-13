12e12302801c7ab04c506fb459189fb57d927094
Yintc <yiqazwsx123@gmail.com>
2022-04-13 13:04:34 +0800

    test env.

--- database/db.py
+++ database/db.py
@@ -1,13 +1,17 @@
 from mysql.connector.pooling import MySQLConnectionPool
+from dotenv import load_dotenv, dotenv_values
+
+# env=".env.develop"
+env=".env.production"
+load_dotenv()
 
 con_pool=MySQLConnectionPool(
     host="localhost",
     user="root",
-    password="AbC123456",
+    password=dotenv_values(env)["mysql_password"],
     database="taipei_scene",
     pool_name="myPool",
     pool_size=20,
     auth_plugin="mysql_native_password",
-    # port="3400",
-    # password="abc123456"
+    port=dotenv_values(env)["mysql_port"]
 )
